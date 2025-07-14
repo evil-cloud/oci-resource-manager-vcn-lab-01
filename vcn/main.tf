fd9fe4cd14e72e705f058368587f13ff5a570bc3
Jose Reynoso <reynosojose2005@gmail.com>
2025-07-14 11:37:47 -0600

    ops: 07-14 11:37

--- vcn/main.tf
+++ vcn/main.tf
@@ -4,3 +4,12 @@ resource "oci_core_virtual_network" "vcn01" {
   dns_label      = var.dns_label
   display_name   = var.display_name
 }
+
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+  compartment_id = var.compartment_id
+  vcn_id = oci_core_virtual_network.vcn01.vcn_id
+
+  depends_on = [ 
+    oci_core_virtual_network.vcn01 
+  ]
+}
